b763cf60f7dd0888131a14771aec42c34da3e06a
aaa <minuhub@gmail.com>
2020-05-03 00:14:28 +0900

    restart

--- app.js
+++ app.js
@@ -1,26 +1,53 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('./config/passport');
+var util = require('./util');
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// DB setting
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect('mongodb+srv://minuhub:tomato@cluster0-qi7ul.mongodb.net/test?retryWrites=true&w=majority');
+var db = mongoose.connection;
+db.once('open', function(){
+  console.log('DB connected'); });
+db.on('error', function(err){
+  console.log('DB ERROR : ', err); });
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+  // Other settings
+  app.set('view engine', 'ejs');
+  app.use(express.static(__dirname+'/public'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended:true}));
+  app.use(methodOverride('_method'));
+  app.use(flash());
+  app.use(session({secret:'MySecret', resave:true, saveUninitialized:true}));
+
+  // Passport
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  // Custom Middlewares
+  app.use(function(req,res,next){
+    res.locals.isAuthenticated = req.isAuthenticated();
+    res.locals.currentUser = req.user;
+    res.locals.util = util;
+    next();
+  });
+
+  // Routes
+  app.use('/', require('./routes/home'));
+  app.use('/posts', util.getPostQueryString, require('./routes/posts'));
+  app.use('/users', require('./routes/users'));
+  app.use('/comments', util.getPostQueryString, require('./routes/comments'));
+  app.use('/files', require('./routes/files'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
